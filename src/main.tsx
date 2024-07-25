843f58ebe0d1a27852f796b4d75b8991c5c03c06
Evgen Grushchenko <evgen.tm959@gmail.com>
2024-07-25 19:52:15 +0200

    projec_03_ReaktRedux_2

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,19 @@
-import React from "react"
 import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import App from "./App"
-import { store } from "./app/store"
-import "./index.css"
+import { store } from "./store/store"
+import GlobalStyles from "./styes/GlobalStyles";
 
 const container = document.getElementById("root")
 
 if (container) {
   const root = createRoot(container)
 
-  root.render(
-    <React.StrictMode>
+  root.render(    
       <Provider store={store}>
+        <GlobalStyles/>
         <App />
       </Provider>
-    </React.StrictMode>,
   )
 } else {
   throw new Error(
